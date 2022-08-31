846e8abc37a53963a5575530f779583fdcb78894
Cmdeejay <charmyminchen@gmail.com>
2022-08-31 08:01:04 +0400

    initial commit

--- main.py
+++ main.py
@@ -1,20 +1,25 @@
 # Bring in dependencies
 from fastapi import FastAPI
 from pydantic import BaseModel
-import keras
 from numpy import asarray
+import keras
+
+
+class CNN:
+    def __init__(self):
+        self.model = keras.models.load_model('32x0x3-ID_NonID_CNN.model')
 
 
 class Item(BaseModel):
     ima: list
 
 
+Classifier = CNN()
 app = FastAPI()
 categories = ["Chinese ID FRONT", "Chinese ID BACK", 'Not Chinese ID']
-model = keras.models.load_model('32x0x3-ID_NonID_CNN.model')
 
 
 @app.post('/')
 async def endpoint(item: Item):
-    yhat = model.predict([asarray(item.ima)]).tolist()
+    yhat = Classifier.model.predict([asarray(item.ima)]).tolist()
     return {'prediction': categories[yhat[0].index(1)]}
